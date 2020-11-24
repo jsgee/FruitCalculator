475fe42ea0a4918ef5d1b14c0bbf28610ea1fb9e
jonathan grimm <jonat@DESKTOP-MQ1DA35>
2020-11-24 15:38:46 -0700

    Add project files.

---
+++ FruitCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FruitCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
